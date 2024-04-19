79121622ebd4f9db2119ff2e5b9e6639e724c8a4
museloper <nosf6842@naver.com>
2024-04-19 11:32:56 +0900

    Middleware & Response

--- src/server.js
+++ src/server.js
@@ -6,7 +6,18 @@ const IP = ip.address()
 
 const app = express()
 
-app.get('/', () => console.log('Somebody is trying to go root.'))
+// S: 미들웨어
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+}
+// E: 미들웨어
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+  return res.send('Hello, world!')
+})
 
 app.listen(PORT, () =>
   console.log(`
