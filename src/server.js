043895dec36057597850622333c0295a2ae46e85
museloper <nosf6842@naver.com>
2024-04-19 11:52:37 +0900

    logger - morgan

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 import express from 'express'
 import ip from 'ip'
+import morgan from 'morgan'
 
 const PORT = 4000
 const IP = ip.address()
 
 const app = express()
+const logger = morgan('dev') // 개발자 레벨 로그
 
 // S: 미들웨어
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`)
-  next()
-}
-// E: 미들웨어
-
 app.use(logger)
+// E: 미들웨어
 
 app.get('/', (req, res) => {
   return res.send('Hello, world!')
