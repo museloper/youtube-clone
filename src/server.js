679114a66453db3037094c68a119fdd4af622ac4
museloper <nosf6842@naver.com>
2025-02-12 11:39:12 +0900

    Challenges Day 03 : #3 middlewares

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 
 // Load environment variables
 dotenv.config()
 
 const app = express()
+const logger = morgan('dev')
 
 const PORT = process.env.PORT
 
+app.use(logger)
 app.get('/', (req, res) => {
   res.send('this is the home page')
 })
