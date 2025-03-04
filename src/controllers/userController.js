6e78fe70c29ea24fe105e5a53e3d8d8364cbb1f4
museloper <nosf6842@naver.com>
2025-03-04 19:07:16 +0900

    github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import fetch from 'node-fetch'
 
 import User from '../models/User'
 
@@ -51,7 +52,7 @@ const postLogin = async (req, res) => {
   const title = 'Login'
   const { username, password } = req.body
 
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ username, socialOnly: false })
   if (!user) {
     return res.status(400).render('user/login', {
       title,
@@ -74,7 +75,8 @@ const postLogin = async (req, res) => {
 }
 
 const logout = (req, res) => {
-  res.send('this is the logout page')
+  req.session.destroy()
+  res.redirect('/')
 }
 
 const view = (req, res) => {
@@ -85,8 +87,99 @@ const edit = (req, res) => {
   res.send('this is the user-edit page')
 }
 
-const remove = (req, res) => {
-  res.send('this is the user-remove page')
+const redirectGithubLogin = (req, res) => {
+  const config = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    allow_signup: false,
+    scope: 'read:user user:email',
+  }
+
+  const params = new URLSearchParams(config).toString()
+
+  const url = `https://github.com/login/oauth/authorize?${params}`
+
+  return res.redirect(url)
 }
 
-export { getJoin, postJoin, getLogin, postLogin, logout, view, edit, remove }
+const handleGithubLogin = async (req, res) => {
+  const config = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code: req.query.code,
+  }
+
+  const params = new URLSearchParams(config).toString()
+
+  const url = `https://github.com/login/oauth/access_token?${params}`
+
+  const accessToken = await (
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+  ).json()
+
+  if ('access_token' in accessToken) {
+    const { access_token } = accessToken
+
+    const apiUrl = 'https://api.github.com'
+
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json()
+
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json()
+
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    )
+
+    if (!emailObj) {
+      return res.redirect('/login')
+    }
+
+    let user = await User.findOne({ email: emailObj.email })
+
+    if (!user) {
+      user = await User.create({
+        email: emailObj.email,
+        avatarUrl: userData.avatar_url,
+        username: userData.login,
+        password: '',
+        name: userData.name,
+        location: userData.location,
+        socialOnly: true,
+      })
+    }
+
+    req.session.loggedIn = true
+    req.session.user = user
+    return res.redirect('/')
+  } else {
+    return res.redirect('/login')
+  }
+}
+
+export {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+  logout,
+  view,
+  edit,
+  redirectGithubLogin,
+  handleGithubLogin,
+}
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { view, edit, remove } from '../controllers/userController'
+import {
+  view,
+  edit,
+  redirectGithubLogin,
+  handleGithubLogin,
+} from '../controllers/userController'
 
 const ur = express.Router()
 
 ur.get('/:id(\\d+)', view)
 ur.get('/edit', edit)
-ur.get('/remove', remove)
+ur.get('/github/open', redirectGithubLogin)
+ur.get('/github/callback', handleGithubLogin)
 
 export default ur
